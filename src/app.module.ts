6765a0eecd8b694ab1c9ee3f9668371103fec9f0
Aleksandr Levus <aleksandr.l@ati.su>
2023-02-04 18:50:17 +0300

    Create CRUD modules: User, Artist, Album, Track, Favorites

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TrackModule,
+    UserModule,
+    ArtistModule,
+    FavoritesModule,
+    AlbumModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
